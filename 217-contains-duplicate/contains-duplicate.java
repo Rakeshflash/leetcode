5f989493498774a89ab94c7275d986e10f513421
Rakeshflash <152472272+Rakeshflash@users.noreply.github.com>
2025-08-04 23:10:40 +0530

    Time: 13 ms (89.11%) | Memory: 58.4 MB (45.71%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true; // Duplicate found
+            }
+            seen.add(num);
+        }
+        return false; // No duplicates
+    }
+}
